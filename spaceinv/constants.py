4464f2a1ae88e78927f99d6461df532be080d1d0
TeddyTjoe1990 <her21059@byui.edu>
2022-03-31 11:46:35 +0700

    change the constants

--- spaceinv/constants.py
+++ spaceinv/constants.py
@@ -1,15 +1,7 @@
-import random
 from game.casting.color import Color
-from os.path import abspath, dirname
-from random import choice
-
-BASE_PATH = abspath(dirname(__file__))
-FONT_PATH = BASE_PATH + '/fonts/'
-IMAGE_PATH = BASE_PATH + '/images/'
-SOUND_PATH = BASE_PATH + '/sounds/'
 
 # GAME
-GAME_NAME = "Space Invaders"
+GAME_NAME = "space invaders"
 FRAME_RATE = 60
 
 # SCREEN
@@ -57,7 +49,7 @@ WHITE = Color(255, 255, 255)
 # KEYS
 LEFT = "left"
 RIGHT = "right"
-SPACE = "fire"
+SPACE = "space"
 ENTER = "enter"
 PAUSE = "p"
 
@@ -72,3 +64,68 @@ GAME_OVER = 4
 LEVEL_FILE = "batter/assets/data/level-{:03}.txt"
 BASE_LEVELS = 5
 
+# -------------------------------------------------------------------------------------------------- 
+# SCRIPTING CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# PHASES
+INITIALIZE = 0
+LOAD = 1
+INPUT = 2
+UPDATE = 3
+OUTPUT = 4
+UNLOAD = 5
+RELEASE = 6
+
+# -------------------------------------------------------------------------------------------------- 
+# CASTING CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# STATS
+STATS_GROUP = "stats"
+DEFAULT_LIVES = 3
+MAXIMUM_LIVES = 5
+
+# HUD
+HUD_MARGIN = 15
+LEVEL_GROUP = "level"
+LIVES_GROUP = "lives"
+SCORE_GROUP = "score"
+LEVEL_FORMAT = "LEVEL: {}"
+LIVES_FORMAT = "LIVES: {}"
+SCORE_FORMAT = "SCORE: {}"
+
+# SHIP
+SHIP_GROUP = "ships"
+SHIP_IMAGE = "spaceinv/assets/images/ship.png"
+SHIP_WIDTH = 28
+SHIP_HEIGHT = 28
+SHIP_RATE = 6
+SHIP_VELOCITY = 6
+
+# SHOOT 
+SHOOT_GROUP = "shoots"
+SHOOT_IMAGES = "spaceinv/assets/images/laser.png" 
+SHOOT_WIDTH = 28
+SHOOT_HEIGHT = 14
+SHOOT_RATE = 6
+SHOOT_VELOCITY = 7
+
+# ENEMY
+ENEMY_GROUP = "enemies"
+ENEMY_IMAGES = {
+    "b": [f"spaceinv/assets/images/{i:03}.png" for i in range(4, 6)],
+    "g": [f"spaceinv/assets/images/{i:03}.png" for i in range(7, 9)],
+    "p": [f"spaceinv/assets/images/{i:03}.png" for i in range(1, 3)]
+}
+ENEMY_WIDTH = 28
+ENEMY_HEIGHT = 28
+ENEMY_RATE = 4
+ENEMY_VELOCITY = 7
+ENEMY_POINTS = 50
+
+# DIALOG
+DIALOG_GROUP = "dialogs"
+ENTER_TO_START = "PRESS ENTER TO START"
+PREP_TO_LAUNCH = "PREPARING TO LAUNCH"
+WAS_GOOD_GAME = "GAME OVER"

d753c9edcc437cb14c121aa68ed18b6f8a58b0cc
TeddyTjoe1990 <her21059@byui.edu>
2022-03-31 14:40:23 +0700

    updated

---
+++ spaceinv/game/scripting/control_ship_action.py
@@ -0,0 +1,16 @@
+from constants import *
+from game.scripting.action import Action
+
+class ControlShipAction(Action):
+
+    def __init__(self, keyboard_service):
+        self._keyboard_service = keyboard_service
+        
+    def execute(self, cast, script, callback):
+        ship = cast.get_first_actor(SHIP_GROUP)
+        if self._keyboard_service.is_key_down(LEFT): 
+            ship.move_left()
+        elif self._keyboard_service.is_key_down(RIGHT): 
+            ship.move_right()  
+        else: 
+            ship.stop_moving()        
